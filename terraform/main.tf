95fa9595e948dfea517aaa3ef1d812ddb821378a
hg22abe <hg22abe@herts.ac.uk>
2025-04-14 00:26:06 +0500

    updated the postgres version to 16 due to 12 being discarded by AWS rds in the future

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,47 @@
-provider "aws" {
-  region = var.region
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.25.2"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.83.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.2"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.4"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket-1742982420"
+    key            = "crypto-system/terraform.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+  }
 }
+
+data "aws_availability_zones" "available" {}
+
+data "aws_secretsmanager_secret_version" "rds_credentials" {
+  secret_id = aws_secretsmanager_secret.rds_credentials.id
+}
+
+data "aws_db_instance" "existing_rds" {
+  db_instance_identifier = "crypto-db"
+}
+
+data "aws_secretsmanager_secret" "existing_secret" {
+  name = "rds-credentials"
+}
+
