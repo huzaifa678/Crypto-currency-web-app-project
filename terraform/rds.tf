33898a5af890d64a15e34bcb74257cf039fb3711
huzaifa678 <huzaifaahmad2210@gmail.com>
2025-09-16 01:02:45 +0500

    changed terraform code to for rds

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -28,7 +28,7 @@ resource "aws_db_instance" "postgres" {
   password = local.rds_db_password
 
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
-  vpc_security_group_ids = [aws_security_group.rds.id]
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
   skip_final_snapshot = true
 
