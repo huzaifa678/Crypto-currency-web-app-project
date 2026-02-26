0bd327789af2b76a209394a58d41e59196c894e0
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-02-26 22:52:51 +0500

    updated the terraform code the vpc and eks cluster file

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -12,6 +12,16 @@ module "vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
 
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+
   tags = {
       Name = "crypto-based-web-system-vpc-network"
   }
