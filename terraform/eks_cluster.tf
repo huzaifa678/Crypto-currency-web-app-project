224dcdc15c4fd07d4a1f2a897be6d3957fd6539f
huzaifa678 <huzaifaahmad2210@gmail.com>
2026-03-02 18:35:43 +0500

    added ingress and egress for https

--- terraform/eks_cluster.tf
+++ terraform/eks_cluster.tf
@@ -65,6 +65,26 @@ resource "aws_security_group" "redis_sg" {
   }
 }
 
+resource "aws_security_group_rule" "allow_nlb_to_nodes_https" {
+  count    = var.environment == "post-test" ? 1 : 0
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_nodes.id
+  source_security_group_id = aws_security_group.eks_nodes.id
+}
+
+resource "aws_security_group_rule" "eks_nodes_egress_https" {
+  count    = var.environment == "post-test" ? 1 : 0
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.eks_nodes.id
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group_rule" "allow_node_to_core_dns_tcp" {
   type                     = "ingress"
   from_port                = 53
